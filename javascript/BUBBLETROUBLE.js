ede57a7a0d86bcd79fbc3f6479880b8acb1604d8
Sölvi Már Magnússon <32644348+solvimar@users.noreply.github.com>
2017-10-24 14:34:08 +0000

    Smá breytingar

---
+++ javascript/BUBBLETROUBLE.js
@@ -0,0 +1,197 @@
+// ==============
+// BUBBLE TROUBLE
+// ==============
+
+"use strict";
+
+/* jshint browser: true, devel: true, globalstrict: true */
+
+var g_canvas = document.getElementById("myCanvas");
+var g_ctx = g_canvas.getContext("2d");
+
+/*
+0        1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+*/
+
+// ====================
+// CREATE INITIAL SHIPS
+// ====================
+
+function createInitialShips() {
+  entityManager.generateShip({
+    cx: 200,
+    cy: 200
+  });
+}
+
+// =============
+// GATHER INPUTS
+// =============
+
+function gatherInputs() {
+  // Nothing to do here!
+  // The event handlers do everything we need for now.
+}
+
+// =================
+// UPDATE SIMULATION
+// =================
+
+// We take a very layered approach here...
+//
+// The primary `update` routine handles generic stuff such as
+// pausing, single-step, and time-handling.
+//
+// It then delegates the game-specific logic to `updateSimulation`
+
+// GAME-SPECIFIC UPDATE LOGIC
+
+function updateSimulation(du) {
+  processDiagnostics();
+
+  entityManager.update(du);
+
+  // Prevent perpetual firing!
+  eatKey(Ship.prototype.KEY_FIRE);
+}
+
+// GAME-SPECIFIC DIAGNOSTICS
+
+var g_allowMixedActions = true;
+var g_useGravity = false;
+var g_useAveVel = true;
+var g_renderSpatialDebug = false;
+var g_useExpl = false;
+
+var KEY_MIXED = keyCode("M");
+var KEY_GRAVITY = keyCode("G");
+var KEY_AVE_VEL = keyCode("V");
+var KEY_SPATIAL = keyCode("X");
+
+var KEY_HALT = keyCode("H");
+var KEY_RESET = keyCode("R");
+
+var KEY_0 = keyCode("0");
+
+var KEY_1 = keyCode("1");
+var KEY_2 = keyCode("2");
+
+var KEY_K = keyCode("K");
+
+var KEY_EXPL = keyCode("E");
+
+function processDiagnostics() {
+  if (eatKey(KEY_MIXED)) g_allowMixedActions = !g_allowMixedActions;
+
+  if (eatKey(KEY_GRAVITY)) g_useGravity = !g_useGravity;
+
+  if (eatKey(KEY_AVE_VEL)) g_useAveVel = !g_useAveVel;
+
+  if (eatKey(KEY_SPATIAL)) g_renderSpatialDebug = !g_renderSpatialDebug;
+
+  if (eatKey(KEY_HALT)) entityManager.haltShips();
+
+  if (eatKey(KEY_RESET)) entityManager.resetShips();
+
+  if (eatKey(KEY_0)) entityManager.toggleRocks();
+
+  if (eatKey(KEY_1))
+    entityManager.generateShip({
+      cx: g_mouseX,
+      cy: g_mouseY,
+
+      sprite: g_sprites.ship
+    });
+
+  if (eatKey(KEY_2))
+    entityManager.generateShip({
+      cx: g_mouseX,
+      cy: g_mouseY,
+
+      sprite: g_sprites.ship2
+    });
+
+  if (eatKey(KEY_K)) entityManager.killNearestShip(g_mouseX, g_mouseY);
+
+  if (eatKey(KEY_EXPL)) g_useExpl = !g_useExpl;
+}
+
+// =================
+// RENDER SIMULATION
+// =================
+
+// We take a very layered approach here...
+//
+// The primary `render` routine handles generic stuff such as
+// the diagnostic toggles (including screen-clearing).
+//
+// It then delegates the game-specific logic to `gameRender`
+
+// GAME-SPECIFIC RENDERING
+function renderSimulation(ctx) {
+  entityManager.render(ctx);
+
+  if (g_renderSpatialDebug) spatialManager.render(ctx);
+}
+
+// =============
+// PRELOAD STUFF
+// =============
+
+var g_images = {};
+
+function requestPreloads() {
+  var requiredImages = {
+    ship: "https://notendur.hi.is/~pk/308G/images/ship.png",
+    ship2: "https://notendur.hi.is/~pk/308G/images/ship_2.png",
+    rock: "https://notendur.hi.is/~pk/308G/images/rock.png",
+    bullet: "sprites/lb.png",
+    explosion: "sprites/Effect95.png"
+  };
+
+  imagesPreload(requiredImages, g_images, preloadDone);
+}
+
+var g_sprites = {};
+var test = 0;
+
+function preloadDone() {
+  g_sprites.ship = new Sprite(g_images.ship);
+  g_sprites.ship2 = new Sprite(g_images.ship2);
+  g_sprites.rock = new Sprite(g_images.rock);
+
+  g_sprites.bullet = new Sprite(g_images.ship);
+  g_sprites.bullet.scale = 0.25;
+
+  // create sprites for the explosion
+  g_sprites.explosion = [
+    new Sprite(g_images.explosion, 0, 0, 128, 128),
+    new Sprite(g_images.explosion, 128, 0, 128, 128),
+    new Sprite(g_images.explosion, 256, 0, 128, 128),
+    new Sprite(g_images.explosion, 384, 0, 128, 128),
+
+    new Sprite(g_images.explosion, 0, 128, 128, 128),
+    new Sprite(g_images.explosion, 128, 128, 128, 128),
+    new Sprite(g_images.explosion, 256, 128, 128, 128),
+    new Sprite(g_images.explosion, 384, 128, 128, 128),
+
+    new Sprite(g_images.explosion, 0, 256, 128, 128),
+    new Sprite(g_images.explosion, 128, 256, 128, 128),
+    new Sprite(g_images.explosion, 256, 256, 128, 128),
+    new Sprite(g_images.explosion, 384, 256, 128, 128),
+
+    new Sprite(g_images.explosion, 0, 384, 128, 128),
+    new Sprite(g_images.explosion, 128, 384, 128, 128),
+    new Sprite(g_images.explosion, 256, 384, 128, 128),
+    new Sprite(g_images.explosion, 384, 384, 128, 128)
+  ];
+  g_sprites.explosion.scale = 0.1;
+  entityManager.init();
+  createInitialShips();
+
+  main.init();
+}
+
+// Kick it off
+requestPreloads();
